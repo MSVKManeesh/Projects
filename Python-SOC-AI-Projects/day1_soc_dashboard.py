ebf4036fcfb3d6fb6aed04aa6674836fa320c529
MSVKManeesh <106618010+MSVKManeesh@users.noreply.github.com>
2026-05-31 20:22:37 +0530

    Add files via upload

---
+++ Python-SOC-AI-Projects/day1_soc_dashboard.py
@@ -0,0 +1,14 @@
+name = "Maneesh"
+role = "SOC Analyst"
+company = "Siemens Hyderabad"
+tool = "Splunk"
+certification = "Cisco Python Essentials"
+goal = "AI Security Automation"
+
+print("Welcome to the SOC Dashboard!")
+print("Analyst:", name)
+print("Role:", role)
+print("Company:", company)
+print("Tool:", tool)
+print("Certification:", certification)
+print("Goal:", goal)
